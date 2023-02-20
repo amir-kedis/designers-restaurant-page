ec1dc17a53ab4e56abdafec04230849e63a9e41f
amir-kedis <amir.kedis@gmail.com>
2023-02-20 21:12:23 +0200

    create text div in home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,9 +4,33 @@ export default function createHome() {
   const home = document.createElement("main");
   home.classList.add("home");
 
-  home.innerHTML = `
-    
-  `;
+  const container = document.createElement("div");
+  container.classList.add("container");
+
+  const text = document.createElement("div");
+  text.classList.add("text");
+
+  const textContentArr = [
+    "EAT WHAT YOUR HEART DESIRES",
+    "IN THE PLACE YOUR HEART",
+    "DESIRES",
+  ];
+
+  textContentArr.forEach((txt, id) => {
+    const pEl = document.createElement("p");
+    pEl.textContent = txt;
+    text.appendChild(pEl);
+  });
+
+  container.appendChild(text);
+
+  const fancyBtn = document.createElement("a");
+  fancyBtn.textContent = "LET'S EAT";
+  fancyBtn.classList.add("btn", "btn--fancy");
+
+  text.appendChild(fancyBtn);
+
+  home.appendChild(container);
 
   return home;
 }
