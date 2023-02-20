20c0d95369d32b651250dead468f87eb5d6c9823
amir-kedis <amir.kedis@gmail.com>
2023-02-20 20:43:28 +0200

    make footer a native js component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,16 +1,22 @@
 import "./footer.scss";
 
 export default function createFooter() {
+  
   const footer = document.createElement("footer");
-  footer.innerHTML = `
-        <div class="container">
-            <p>
-                DESIGNED BY Amir_Kedis
-            </p>
-            <a href="https://github.com/amir-kedis" target="_blank" class="btn btn--check">
-                CHECK ME OUT
-            </a>
-        </div>
-    `;
+  const container = document.createElement("div");
+  container.classList.add("container");
+  const footerP = document.createElement("p");
+  footerP.textContent = "DESIGNED BY Amir_Kedis";
+  const btn = document.createElement("a");
+  btn.classList.add("btn");
+  btn.classList.add("btn--check");
+  btn.href = "https://github.com/amir-kedis";
+  btn.textContent = "CHECK ME OUT";
+
+  container.appendChild(footerP);
+  container.appendChild(btn);
+
+  footer.appendChild(container);
+
   return footer;
 }
