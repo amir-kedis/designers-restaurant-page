5c3688e98485767b2a01ad042f9dddbdc966b238
amir-kedis <amir.kedis@gmail.com>
2023-04-08 08:41:01 +0200

    display menu component instead of home

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import "./style/layout.scss";
 import createHeader from "./components/header/header";
 import createFooter from "./components/footer/footer";
 import createHome from "./components/home/home";
+import createMenu from "./components/menu/menu";
 
 const header = createHeader();
 const footer = createFooter();
 const home = createHome();
+const menu = createMenu();
 
 document.querySelector("#content").appendChild(header);
-document.querySelector("#content").appendChild(home);
+//document.querySelector("#content").appendChild(home);
+document.querySelector("#content").appendChild(menu);
 document.querySelector("#content").appendChild(footer);
