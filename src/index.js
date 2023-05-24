fb8d8858e6f963fb856da6aab081d9f86274cc73
amir-kedis <amir.kedis@gmail.com>
2023-05-24 20:23:59 +0300

    🌟 change pages with mouse clicks

--- src/index.js
+++ src/index.js
@@ -7,12 +7,100 @@ import createFooter from "./components/footer/footer";
 import createHome from "./components/home/home";
 import createMenu from "./components/menu/menu";
 
-const header = createHeader();
-const footer = createFooter();
-const home = createHome();
-const menu = createMenu();
+// current visible page
+const pages = {
+  HOME: 0,
+  MENU: 1,
+  ABOUT: 2
+}
 
-document.querySelector("#content").appendChild(header);
-//document.querySelector("#content").appendChild(home);
-document.querySelector("#content").appendChild(menu);
-document.querySelector("#content").appendChild(footer);
+let currnetPage = pages.HOME;
+
+function changePage(e)
+{
+  console.log(e.target.textContent);
+  
+  let clickItem = e.target.textContent;
+
+  if (clickItem.toUpperCase() == "HOME")
+  {
+    currnetPage = pages.HOME; 
+  } else if (clickItem.toUpperCase() == "ABOUT US") 
+  { 
+    currnetPage = pages.ABOUT; 
+  } else if (clickItem.toUpperCase() == "MENU")
+  {
+    currnetPage = pages.MENU;
+  } else {
+    currnetPage = pages.HOME; 
+  }
+}
+
+
+function render() {
+  const contentDiv = document.querySelector('#content');
+
+  // remove existing divs if they exist 
+  if (contentDiv.querySelector('main'))
+  {
+    contentDiv.removeChild(contentDiv.querySelector('main'));
+  }
+  
+  if (contentDiv.querySelector('footer'))
+  {
+    contentDiv.removeChild(contentDiv.querySelector('footer'));
+  }
+
+  
+  const footer = createFooter();
+  
+  let header = contentDiv.querySelector('header');
+
+  if (!header)
+  {
+    header = createHeader();
+    contentDiv.appendChild(header);
+  
+
+  // add event listner
+  const links = header.querySelectorAll('a');
+
+  console.log(links);
+
+  [...links].forEach(link => {
+    console.log(link);
+    link.addEventListener('click', (e) => {
+      changePage(e);
+      render();
+    }); 
+  });
+
+  }
+
+  switch (currnetPage)
+  {
+    case pages.HOME:
+      const home = createHome();
+      contentDiv.appendChild(home);
+      break;
+    case pages.MENU:
+      const menu = createMenu();
+      contentDiv.appendChild(menu);
+      break;
+    case pages.ABOUT:
+      // TODO: uncomment latter
+//      const about = createAbout();
+//      contentDiv.appendChild(about);
+      break;
+    default: 
+      const homeD = createHome();
+      contentDiv.appendChild(homeD);
+      break;
+  }
+
+  contentDiv.appendChild(footer);
+}
+
+render();
+
+
